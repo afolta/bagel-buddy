3a03b4554636afbd6e9e44e705a106d7a776b83c
Adam Folta <afolta@thecitybase.com>
2022-12-30 12:48:19 -0500

    a user can see which places they have visited

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,18 @@
+class TripsController < ApplicationController
+  def create
+    trip = Trip.create(
+      user_id: params[:user_id],
+      restaurant_id: params[:restaurant_id],
+    )
+    render json: trip.as_json
+  end
+
+  def show
+    trip = current_user.trips.find_by(id: params[:id])
+    if trip
+      render json: trip.as_json
+    else
+      render json: { message: "This is not your trip! (Or order doesn't exist)" }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   get "/restaurants" => "restaurants#index"
+
+  get "/reviews" => "reviews#index"
+
+  get "/trips/:id" => "trips#show"
+  post "/trips" => "trips#create"
 end
