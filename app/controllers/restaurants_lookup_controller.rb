4f77162049b0c0ba351882a14ead115883889333
Adam Folta <afolta@thecitybase.com>
2023-01-01 18:14:27 -0500

    add lookup url refactor

--- app/controllers/restaurants_lookup_controller.rb
+++ app/controllers/restaurants_lookup_controller.rb
@@ -4,37 +4,45 @@ class RestaurantsLookupController < ApplicationController
 
   def new
     # Refactor variables
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-    radius = params[:radius] || 8047 ## Defaults to 5 mile radius
-    keyword = "bagel"
+    @latitude = params[:latitude]
+    @longitude = params[:longitude]
+    @radius = params[:radius] || 8047 ## Defaults to 5 mile radius
+    @keyword = "bagel"
 
-    RestaurantLookupRequest.create!(lookup_parameters: { latitude: latitude, longitude: longitude, radius: radius, keyword: keyword })
+    RestaurantLookupRequest.create!(lookup_parameters: { latitude: @latitude, longitude: @longitude, radius: @radius, keyword: @keyword })
 
-    url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{latitude}%2C#{longitude}&radius=#{radius}&type=restaurant&keyword=#{keyword}&key=#{Rails.application.credentials.google_places_api[:api_key]}")
-
-    https = Net::HTTP.new(url.host, url.port)
+    https = Net::HTTP.new(lookup_url.host, lookup_url.port)
     https.use_ssl = true
 
-    request = Net::HTTP::Get.new(url)
+    request = Net::HTTP::Get.new(lookup_url)
     response = https.request(request)
     lookup_response = JSON.parse(response.read_body)
 
     restaurants = lookup_response["results"].map do |restaurant|
       {
         name: restaurant["name"],
+        place_id: restaurant["place_id"],
+        address: restaurant["vicinity"],
         latitude: restaurant["geometry"]["location"]["lat"],
         longitude: restaurant["geometry"]["location"]["lng"],
+        rating: restaurant["rating"],
+        user_ratings_total: restaurant["user_ratings_total"],
       }
     end
-
-    puts "Restaurant Names are #{restaurants}"
 
     # Need to find a way to link to the lookup
     RestaurantLookupResponse.create!(lookup_response: lookup_response[:results])
 
-    render json: lookup_response
-    # Keep for debugging purposes
+    render json: restaurants
+
+    puts "\n== LOOKUP RESULTS =="
     puts lookup_response[:results]
+
+    puts "\n== RESTAURANT INFO =="
+    puts "Restaurant Names are #{restaurants}"
   end
+end
+
+def lookup_url
+  @lookup_url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@latitude}%2C#{@longitude}&radius=#{@radius}&type=restaurant&keyword=#{@keyword}&key=#{Rails.application.credentials.google_places_api[:api_key]}")
 end
