8f81072fe5e432b792f4b025bcdb4400c3686d0c
Adam Folta <afolta@thecitybase.com>
2023-01-08 10:53:01 -0500

    Add trip deletion

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,8 +1,10 @@
 class TripsController < ApplicationController
   def create
-    trip = Trip.create(
-      user_id: params[:user_id],
+    trip = Trip.create!(
+      user_id: current_user.id,
       restaurant_id: params[:restaurant_id],
+      place_id: params[:place_id],
+      friendly_date: Time.now.strftime("%m/%d/%Y"),
       notes: params[:notes],
     )
     render json: trip.as_json
@@ -20,5 +22,12 @@ class TripsController < ApplicationController
     else
       render json: { notes: "There are no notes for this place." }
     end
+  end
+
+  def destroy
+    trip_id = params[:id]
+    trip = Trip.find(trip_id)
+    trip.destroy
+    render json: { message: "Trip successfully destroyed!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   get "/trips/:id" => "trips#trips_by_place_id"
   post "/trips" => "trips#create"
 
+  delete "/trips/:id" => "trips#destroy"
+
   post "/restaurants-lookup" => "restaurants_lookup#new"
 end
