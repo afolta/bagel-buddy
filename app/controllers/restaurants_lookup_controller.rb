58788e24a40788159edeb36551614b3c23dfa74c
Adam Folta <afolta@thecitybase.com>
2023-01-25 13:54:33 -0500

    initial restaurants lookup controller refactor

--- app/controllers/restaurants_lookup_controller.rb
+++ app/controllers/restaurants_lookup_controller.rb
@@ -4,22 +4,51 @@ class RestaurantsLookupController < ApplicationController
   require "geocoder"
 
   def new
-    # Refactor variables
+    create_restaurant_lookup = restaurant_lookup_request
+
+    https = Net::HTTP.new(lookup_url.host, lookup_url.port)
+    https.use_ssl = true
+    request
+
+    puts "\n== LOOKUP RESULTS =="
+    puts lookup_response[:results]
+
+    puts "\n== RESTAURANT INFO =="
+    puts "Restaurant Names are #{restaurants}"
+  end
+
+  private def request
+    Net::HTTP::Get.new(lookup_url)
+  end
+
+  private def lookup_response(request)
+    response = https.request(request)
+    JSON.parse(response.read_body)
+  end
+
+  def lookup_url
     @latitude = params[:latitude]
     @longitude = params[:longitude]
     @radius = params[:radius] || 8047 ## Defaults to 5 mile radius
     @keyword = "bagel"
 
-    RestaurantLookupRequest.create!(lookup_parameters: { latitude: @latitude, longitude: @longitude, radius: @radius, keyword: @keyword })
+    # Can be moved to a config file.
+    @lookup_url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@latitude}%2C#{@longitude}&radius=#{@radius}&type=restaurant&keyword=#{@keyword}&key=#{Rails.application.credentials.google_places_api[:api_key]}")
+  end
 
-    https = Net::HTTP.new(lookup_url.host, lookup_url.port)
-    https.use_ssl = true
+  private def restaurant_lookup_request
+    RestaurantLookupRequest.create!(
+      lookup_parameters: {
+        latitude: params[:latitude],
+        longitude: params[:longitude],
+        radius: params[:radius] || 8047, ## Defaults to 5 mile radius,
+        keyword: "bagel",
+      },
+    )
+  end
 
-    request = Net::HTTP::Get.new(lookup_url)
-    response = https.request(request)
-    lookup_response = JSON.parse(response.read_body)
-
-    restaurants = lookup_response["results"].map do |restaurant|
+  private def shaped_response(response)
+    lookup_response["results"].map do |restaurant|
       {
         name: restaurant["name"],
         place_id: restaurant["place_id"],
@@ -30,23 +59,9 @@ class RestaurantsLookupController < ApplicationController
         user_ratings_total: restaurant["user_ratings_total"],
         distance: Geocoder::Calculations.distance_between([restaurant["geometry"]["location"]["lat"], restaurant["geometry"]["location"]["lng"]], [current_user.latitude, current_user.longitude]).round(2),
       }
+
+      sorted = sort_by { |elem| elem[:distance] }
+      render json: sorted
     end
-
-    # Need to find a way to link to the lookup
-    # RestaurantLookupResponse.create!(lookup_response: lookup_response[:results])
-
-    sorted = restaurants.sort_by { |elem| elem[:distance] }
-
-    render json: sorted
-
-    puts "\n== LOOKUP RESULTS =="
-    puts lookup_response[:results]
-
-    puts "\n== RESTAURANT INFO =="
-    puts "Restaurant Names are #{restaurants}"
   end
-end
-
-def lookup_url
-  @lookup_url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@latitude}%2C#{@longitude}&radius=#{@radius}&type=restaurant&keyword=#{@keyword}&key=#{Rails.application.credentials.google_places_api[:api_key]}")
 end
