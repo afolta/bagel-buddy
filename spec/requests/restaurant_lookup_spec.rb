ff79d37098f171bfc398fd6d7a7f0605defc1d42
Adam Folta <afolta@thecitybase.com>
2023-01-31 13:13:51 -0500

    restaurant lookup spec authentication attempt

--- spec/requests/restaurant_lookup_spec.rb
+++ spec/requests/restaurant_lookup_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "RestaurantLookups", type: :request do
   describe "POST /restaurants-lookup" do
-    let!(:current_user_latitude) { '39.94709795'}
-    let!(:current_user_longitude) { '-75.17313898856435'}
-    let!(:current_user) { User.create }
+    let!(:current_user_latitude) { "39.94709795"}
+    let!(:current_user_longitude) { "-75.17313898856435"}
 
     # it 'A user must have a zip code' do
     #   expect { User.create!(
@@ -22,16 +21,29 @@ RSpec.describe "RestaurantLookups", type: :request do
     # end
 
     before do
+      # post '/sessions.json',
+      # params: { email: "test@test.com", password: "password" }
 
+      # jwt = response.body('jwt')
+
+      post '/restaurants-lookup', params: {
+        latitude: current_user_latitude,
+        longitude: current_user_longitude
+      },
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NzUyNzI1NjV9.FAcB9th3RBg6tQeruUWTrOy0ywBIRJBwuc9ixw_Ti_E',
+        'Content-Type': 'application/json'
+      }
     end
 
     it 'returns an HTTP status of 200' do
-      post '/restaurants-lookup', params:
-                          {
-                            latitude: current_user_latitude,
-                            longitude: current_user_longitude
-                          }
       expect(response).to have_http_status(200)
+    end
+
+    it 'returns a name' do
+      json_response = JSON.parse(response.body)
+
+      json_response.first.dig('name').should_not be_nil
     end
   end
 end
